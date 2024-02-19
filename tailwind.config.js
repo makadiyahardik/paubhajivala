9703b85f18ad89b799861350fc40fefafe5267ef
hardikmakadiya <hardikmakadiya8@gmail.com>
2024-02-19 16:06:25 +0530

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      primary: "#1F0048",
+      skyblue: "#399CC8",
+      blue: "#612ADB",
+      DarkSkyblue: "#3ECEF1",
+      black: "#000000",
+      white: "#FFFFFF",
+    },
+    fontSize: {
+      sm: ["14px", "20px"],
+      base: ["18px", "30px"],
+      lg: ["20px", "28px"],
+      xl: ["24px", "32px"],
+      xxl: ["35px", "43px"],
+    },
+    fontWeight: {
+      hairline: 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+    },
+    extend: {
+      transform: {
+        'rotate-y-20': 'rotateY(20deg)',
+      },
+      perspective: {
+        '1100': '1100px',
+      },
+    },
+  },
+  plugins: [],
+}
