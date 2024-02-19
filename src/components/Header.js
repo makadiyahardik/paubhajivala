364a397ba325eb5bac67f66c44583e0b80995707
hardikmakadiya <hardikmakadiya8@gmail.com>
2024-02-19 18:16:24 +0530

    logo position fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,11 +9,11 @@ function Header() {
   className="pt-4"
         >
         <div className="md:max-w-[1400px] w-full  bg-black md:px-10 px-5 mx-auto ">
-          <div className="flex  justify-between  md:items-center  ">
+          <div className="flex  justify-center  md:items-center  ">
             <div className="flex justify-start items-start">
               <img
                 src="./pblogo.PNG"
-                className="h-[3rem] w-auto "
+                className="h-[4rem] w-auto "
               />
             </div>
           
